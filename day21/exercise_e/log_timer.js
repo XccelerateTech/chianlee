ca51231059bca8810058711ff4320a583bb81dc5
chian88 <leechianyee@gmail.com>
2018-10-29 16:05:27 +0800

    dfoadsk

--- day21/exercise_e/log_timer.js
+++ day21/exercise_e/log_timer.js
@@ -1,12 +1,27 @@
-var timer = require('./timer');
+var Timer = require('./timer');
 
-var clock = new timer(5)
+
+var clock = new Timer(5);
+clock.start();
+setTimeout( () => {
+	clock.pause();
+	clock.start();
+}, 5000);
+
+setTimeout( () => {
+	clock.stop();
+	clock.start();
+}, 9000);
 
 clock.on('tick', function(secondRemaining) {
-	if (secondRemaining > 0) {
-		console.log(secondRemaining);	
-	} else {
-		console.log('kaboom');
-		clearInterval(clock.timer);
-	}
+	console.log(secondRemaining);
+});
+
+
+clock.on('pause', function(secondRemaining) {
+	console.log('i am paused! time remaining is : ' + secondRemaining);
+});
+
+clock.on('stop', function(secondRemaining) {
+	console.log('i am stopped! time remaining is : ' + secondRemaining);
 });
--- day21/exercise_e/timer.js
+++ day21/exercise_e/timer.js
@@ -14,40 +14,21 @@ class Timer extends EventEmitter {
 			self.secondRemaining = self.secondTotal - self.secondPassed;
 			self.emit('tick', self.secondRemaining);
 			self.secondPassed += 1;
-			console.log(self.secondRemaining);
 		}, 1000);
 	}
 
 	pause() {
 		clearInterval(this.timer);
-		console.log('i am pause');
-
-		/*
-			clear interval
-
-		*/
+		this.emit('pause', this.secondRemaining);
 	}
 
 	stop() {
 		clearInterval(this.timer);
 		this.secondPassed = 0;
 		this.secondRemaining = this.secondTotal;
-		console.log('i am stoped');
+		this.emit('stop', this.secondRemaining);
 	}
 }
 
-var clock = new Timer(5);
-clock.start();
-
-setTimeout(function() {
-	clock.pause();
-	clock.start();
-}, 4000);
-
-
-setTimeout(function() {
-	clock.stop();
-	clock.start();
-}, 9000);
 
 module.exports = Timer;
